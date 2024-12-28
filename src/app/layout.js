292e8f288b77b8cd9ad2be394a35fa9df2e35f4d
mowabanga <itsmundia@gmail.com>
2024-12-28 09:55:50 +0300

    Add dark/light mode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "next-themes";
+import ThemeComponent from "./components/ThemeComponent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,12 +23,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Header />
-          {children}
+          <ThemeProvider attribute={"class"}>
+            <Header />
+            {children}
+            <ThemeComponent />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
